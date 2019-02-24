fa7f2160dd39219f9feb8bece407322f15b62d74
Bryce <bryce@brycedev.com>
2019-02-23 21:20:05 -0500

    converted form view to redesign, coffee and pug

--- dapp/src/views/Dash.vue
+++ dapp/src/views/Dash.vue
@@ -8,12 +8,23 @@
             img.rounded-full.w-10.h-10(src="../assets/images/nav_logo.png")
             .ml-auto.flex.items-center
               i.material-icons.text-formsid.opacity-50.subtle.hover-opacity-90.cursor-pointer.mr-4 &#xE7F4;
-              p.text-formsid.font-light.text-md.px-4.py-2.rounded.bg-formsid-clear forms.id
-          .w-full.flex.justify-between.items-center.my-12
-            h1.font-thin.text-formsid {{ pageTitle }}
-            div.flex.items-center.cursor-pointer.bg-formsid-clear.px-4.subtle.hover-bg-formsid.hover-text-white.font-light.text-formsid.py-3.rounded
-              i.material-icons.text-lg.mr-2 create
-              span Create
+              p.text-formsid.font-light.text-md.px-4.py-2.rounded.bg-formsid-clear {{ user ? user.username : 'blockstacker' }}
+          .w-full.flex.justify-between.items-center.my-10
+            h1.font-thin.text-formsid.py-2 {{ pageTitle }}
+            .flex(v-if="$route.name == 'Forms'")
+              div.flex.items-center.cursor-pointer.bg-formsid-clear.px-4.subtle.hover-bg-formsid.hover-text-white.font-light.text-formsid.py-3.rounded
+                i.material-icons.text-lg.mr-2 create
+                span Create
+            .flex(v-if="$route.name == 'Form'")
+              div.flex.items-center.cursor-pointer.border-formsid-clear.border.px-4.subtle.hover-bg-formsid.hover-text-white.font-light.text-formsid.py-3.rounded.mr-4
+                i.material-icons.text-lg.mr-2 remove_red_eye
+                span Preview
+              div.flex.items-center.cursor-pointer.border-formsid-clear.border.px-4.subtle.hover-bg-formsid.hover-text-white.font-light.text-formsid.py-3.rounded.mr-4
+                i.material-icons.text-lg.mr-2 cloud_download
+                span Export Data
+              div.flex.items-center.cursor-pointer.bg-formsid-clear.px-4.subtle.hover-bg-formsid.hover-text-white.font-light.text-formsid.py-3.rounded
+                i.material-icons.text-lg.mr-2 edit
+                span Edit
           .w-full
           .flex
             router-link.flex.items-center.no-underline.px-4.subtle.hover-bg-formsid-clear.font-light.text-formsid.p-2.rounded.mr-6(:to="{ name: 'Dashboard' }" active-class="bg-formsid-clear" exact)
@@ -31,7 +42,7 @@
 <script lang="coffee">
   import Auth from '../components/Auth.vue'
   export default
-    store: ['collections']
+    store: ['collections', 'user']
     components: { Auth }
     data: -> { }
     computed:
