ec8e494f94ff2dd7229e872b00f2ba8982ef2c82
Bryce <bryce@brycedev.com>
2019-03-03 18:35:50 -0500

    remove dashboard link for now from nav

--- dapp/src/views/Dash.vue
+++ dapp/src/views/Dash.vue
@@ -8,7 +8,8 @@
             i.material-icons.text-formsid.subtle.cursor-pointer.mr-4.relative(@click="toggleNotifications") &#xE7F4;
               .absolute.rounded.h-0.subtle.bg-white.shadow-md.pin-r.w-64.z-max.mt-4(:class="{'pointer-events-auto opacity-100 max-h-64 h-auto' : notificationsVisible, 'pointer-events-none opacity-0 h-0' : !notificationsVisible}")
                 .flex.flex-col.w-full
-                  .border.b.py-8.bg-formsid-pale.subtle(v-for="notification in eligibleNotifications")
+                  .border-b.bg-formsid-pale.subtle(v-for="n in eligibleNotifications")
+                    p.py-4.font-light.text-formsid-glass(v-if="n.type == 'response'") has received a new response.
             p.text-formsid.font-light.text-md.px-4.py-2.rounded.bg-formsid-clear {{ user ? user.username : 'blockstacker' }}
         .w-full.flex.justify-between.items-center.my-10
           h1.font-thin.text-formsid.py-2 {{ pageTitle }}
@@ -20,7 +21,7 @@
             a.block.no-underline(:href="formPreview()" target="_blank" v-if="currentForm && currentForm.public")
               div.flex.items-center.cursor-pointer.border-formsid-clear.border.px-4.subtle.hover-bg-formsid.hover-text-white.font-light.text-formsid.py-3.rounded.mr-4
                 i.material-icons.text-lg.mr-2 remove_red_eye
-                span Preview
+                span View
             div.flex.items-center.cursor-pointer.border-formsid-clear.border.px-4.subtle.hover-bg-formsid.hover-text-white.font-light.text-formsid.py-3.rounded.mr-4(@click="bus.$emit('exportdata', $route.params.id)")
               i.material-icons.text-lg.mr-2 cloud_download
               span Export Data
@@ -29,9 +30,9 @@
               span Edit
         .w-full
         .flex
-          router-link.flex.items-center.no-underline.px-4.subtle.hover-bg-formsid-clear.font-light.text-formsid.p-2.rounded.mr-6(:to="{ name: 'Dashboard' }" active-class="bg-formsid-clear" exact)
-            i.material-icons.text-lg.mr-2 dashboard
-            span Dashboard
+          //- router-link.flex.items-center.no-underline.px-4.subtle.hover-bg-formsid-clear.font-light.text-formsid.p-2.rounded.mr-6(:to="{ name: 'Dashboard' }" active-class="bg-formsid-clear" exact)
+          //-   i.material-icons.text-lg.mr-2 dashboard
+          //-   span Dashboard
           router-link.flex.items-center.no-underline.px-4.subtle.hover-bg-formsid-clear.font-light.text-formsid.p-2.rounded.mr-6(:to="{ name: 'Forms' }" active-class="bg-formsid-clear")
             i.material-icons.text-lg.mr-2 filter_none
             span Forms
