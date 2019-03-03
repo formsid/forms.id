d6ce48d3311bba9792171675b2c78e33230b3557
Bryce <bryce@brycedev.com>
2019-03-02 21:27:19 -0500

    plugged in user answers to submission view

--- dapp/src/views/NewForm.vue
+++ dapp/src/views/NewForm.vue
@@ -13,42 +13,30 @@
             .text-center.flex.flex-col
               h4.mb-2.font-light.text-2xl.text-formsid-darkest {{ form.views }}
               span.font-light.text-uppercase.text-formsid-darker Views
-        table.w-full
+        table.w-full.px-8
           thead
             tr
-              th.py-4.font-light.text-formsid-darkest(scope="col") #
-              th.py-4.font-light.text-formsid-darkest(scope="col") Data
-              th.py-4.font-light.text-formsid-darkest(scope="col") Timestamp
+              th.px-4.py-4.font-light.text-formsid-darkest(scope="col") #
+              th.px-4.py-4.font-light.text-formsid-darkest(scope="col") Data
+              th.px-4.py-4.font-light.text-formsid-darkest(scope="col") Timestamp
           tbody(v-if="form.submissions && form.submissions.length")
-            tr.hover-bg-pale-khaki.cursor-pointer(v-for="(record, i) in form.submissions" :key="record._id" @click="rowClicked(record._id)")
-              td {{ i + 1 }}
-              td {{ filled(record.data) }}
-              td {{ timestamp(record.created) }}
+            tr.subtle.border-t.hover-bg-formsid-pale.cursor-pointer(v-for="(record, i) in form.submissions" :key="record._id" @click="rowClicked(record._id)")
+              td.px-4.py-4.font-light.text-formsid-darkest.text-center {{ i + 1 }}
+              td.px-4.py-4.font-light.text-formsid-darkest.text-center Click to view data
+              td.px-4.py-4.font-light.text-formsid-darkest.text-center {{ timestamp(record.created) }}
         p.py-4.text-center.font-light.text-formsid-darker.border-t No submissions, yet.
-      div(class="subtle pin max-h-screen overflow-hidden h-screen fixed w-full bg-light-smoke z-max" @click.self="hideRowView" :class="{'pointer-events-auto opacity-100' : rowViewVisible, 'pointer-events-none opacity-0' : !rowViewVisible}" v-if="form")
+      div(class="subtle pin max-h-screen overflow-hidden h-screen fixed w-full bg-light-smoke z-max" @click.self="hideRowView" :class="{'pointer-events-auto opacity-100' : rowViewVisible, 'pointer-events-none opacity-0' : !rowViewVisible}" v-if="form && row")
         .absolute.pin-r.pin-b.pin-t.w-half.bg-white.shadow-lg
           .absolute.pin-t.pin-x.pin-r.border-t-8.border-formsid.z-max
           .bg-white.h-full.shadow.form-container.overflow-y-scroll.relative
             div.max-w-md.px-4.mx-auto.pb-12(style="margin-top: 5%;")
               div.w-full.mb-16
-                div.py-6.px-8.subtle.bg-formsid-transparent(v-for="obj in form.objects" :key="obj.id")
-                  div.flex.items-center
-                    div.flex-grow
-                      p.break-words.greycliff.max-w-lg.mx-auto.text-left.text-xl.leading-loose.mb-2.text-formsid-darkest(:content.sync="obj.data.title" v-if="obj.data.type !== 'image'") {{ obj.data.title }}
-                      <div class="flex items-center max-w-lg mx-auto py-2" v-if="obj.data.type == 'shortanswer'">
-                        <input type="text" :aria-label="obj.data.title" class="subtle rounded focus:border-b-2 border-formsid-glass bg-formsid-clear appearance-none w-full p-3 leading-tight focus:outline-none outline-none greycliff text-xl font-light text-formsid-glass focus:text-formsid-glass">
-                      </div>
-                      div.flex.items-center.max-w-lg.mx-auto.py-2(v-if="obj.data.type == 'paragraph'")
-                        textarea(aria-label="Full name" class="subtle rounded focus:border-b-2 border-formsid-glass bg-formsid-clear appearance-none w-full p-3 leading-tight focus:outline-none outline-none greycliff text-xl font-light text-formsid-darker focus:text-formsid-glass resize-none" rows="3" v-autosize readonly :value="'hi'")
-                      </div>
-                      <div class="flex items-center max-w-lg mx-auto py-2" v-if="['dropdown', 'multipleanswer', 'multiplechoice'].indexOf(obj.data.type) > -1">
-                        <div class="flex flex-col w-full">
-                          <div class="greycliff bg-formsid-clear rounded text-center p-3 text-lg text-formsid-glass leading-tight tracking-normal cursor-pointer subtle mb-4" v-for="choice in obj.data.choices" :key="choice.label">{{ choice.label }}</div>
-                        </div>
-                      </div>
-                      <div class="flex items-center max-w-lg mx-auto py-2" v-if="obj.data.type == 'image' || obj.data.type == 'imagewlabel'">
-                        <img class="w-full rounded" :src="obj.data.src"/>
-                      </div>
+                div.py-3.px-8.subtle.bg-formsid-transparent(v-for="obj in form.objects" :key="obj.id")
+                  div.flex.flex-col.mb-4(v-if="obj.data.type.indexOf('image') == -1")
+                    p.break-words.greycliff.w-full.text-left.text-xl.leading-loose.mb-2.text-formsid-darkest(:content.sync="obj.data.title") {{ obj.data.title }}
+                    input(type="text" class="subtle rounded focus:border-b-2 border-formsid-glass bg-formsid-clear appearance-none w-full p-3 leading-tight focus:outline-none outline-none greycliff text-xl font-light text-formsid-glass focus:text-formsid-glass" v-if="['email', 'shortanswer'].indexOf(obj.data.type) > -1")
+                    textarea(aria-label="Full name" class="subtle rounded focus:border-b-2 border-formsid-glass bg-formsid-clear appearance-none w-full p-3 leading-tight focus:outline-none outline-none greycliff text-xl font-light text-formsid-darker focus:text-formsid-glass resize-none" rows="3" v-autosize readonly :value="row.data.find(a => a.id == obj.id).answer" v-if="obj.data.type == 'paragraph'")
+                    div(class="w-full greycliff bg-formsid-clear rounded text-center p-3 text-lg text-formsid-glass leading-tight tracking-normal subtle" v-if="['dropdown', 'multipleanswer', 'multiplechoice'].indexOf(obj.data.type) > -1") {{ row.data.find(a => a.id == obj.id).answer }}
 </template>
 
 <script lang="coffee">
@@ -65,12 +53,18 @@ export default
     rowViewVisible: false
     selectedObjectId: null
   computed:
-    answerable: -> o for o in @form.objects when o.data.type isnt 'image' if @form?
+    answerable: -> o for o in @form.objects when o.data.type.indexOf('image') is -1 if @form?
     row: -> (s for s in @form.submissions when s._id is @selectedObjectId)[0] if @selectedObjectId?
   methods:
     exportData: ->
       headers = @form.objects.filter(o) -> o.data.type isnt 'image'.map(a) -> a.data.title
-      ws = XLSX.utils.json_to_sheet(@formData, { header: headers })
+      formData = []
+      for s in newValue.submissions
+        record = {}
+        record[object.data.title] is (a for a in s.data when a.id is object.id)[0] for object in @answerable
+        formData.push(record)
+      console.log formData
+      ws = XLSX.utils.json_to_sheet(formData, { header: headers })
       wb = XLSX.utils.book_new()
       XLSX.utils.book_append_sheet wb, ws, "Responses"
       XLSX.writeFile wb, "#{@form.title}.xlsx"
@@ -81,25 +75,12 @@ export default
         year: 'numeric', month: 'numeric', day: 'numeric',
         hour: 'numeric', minute: 'numeric'
       }).format(new Date(time))
-    filled: (data) ->
-      max = @answerable.length
-      "#{(data.length / max) * 100}% filled"
+    # filled: (data) ->
+    #   "" unless
+    #   "#{(data.length / @answerable.length) * 100}% filled" if data?
     hideRowView: ->
       @rowViewVisible = false
     rowClicked: (id) ->
       @selectedObjectId = id
       @rowViewVisible = true
-  watch:
-    form: (newValue, oldValue) ->
-      if newValue
-        answerable = o for o in newValue.objects when o.data.type isnt 'image'
-        data = []
-        for s in newValue.submissions
-          record = {}
-          aids = a.id for a in answerable
-          for object in aids
-            titleCheck = (o for o in newValue.objects when o.id is object.data.title)[0]
-            record[newValue.objects.find(o) -> o.id == object.data.title] is s.data[object]
-          data.push(record)
-        @formData = data
 </script>
